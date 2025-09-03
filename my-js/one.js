e29081bd92bbac611110b3ed940295f264a45667
alexcavatoraablya <ohrimchuksofi@gmail.com>
2025-09-03 13:46:11 +0300

    Add files via upload

---
+++ my-js/one.js
@@ -0,0 +1,16 @@
+       a = 23; //змінна цілого типу - Number
+        str = "класна погода"; //string - рядок
+        a = "політ в космос";
+        var name = "віктор"; //рядок
+        //вбудована функція, яка може запитувати інформацію
+        name = prompt("скажіть хто ви?");
+        console.log("name = ", name);
+        alert("молодець "+ name)
+        //document.writeln(`<h3 class="text-danger">${name}</h3>`);
+        //спрацьовує коли запускається скрипт - 
+        document.writeln("a = "+ a);
+        document.writeln("<br/>")
+        document.writeln("<b>"+str+"</b>");
+        //вбудована функція
+        //alert("коси траву");
+        console.log("мій перший контроль")
